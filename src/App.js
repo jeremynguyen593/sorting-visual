a03acb3e5d64538fefb3b228b78d9d9cd70143d5
jeremynguyen593 <jeremynugyen593@gmail.com>
2022-11-23 23:10:19 -0800

    Created the basic random array to be sorted. Created the basic layout for the bars and centered it in the middle.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,62 @@
-import React from 'react'
-import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
+import React, {Component} from 'react';
+
+import Bar from './components/Bar';
+
+//CSS imports
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <SortingVisualizer></SortingVisualizer>
-    </div>
-  );
-}
+class App extends Component {
+    state = { 
+        array: [],
+        arraySteps: [],
+        colorKey: [],
+        colorSteps: [],
+        currentStep: 0,
+        count: 10,
+        delay: 100,
+        algorithms: ' ',
+        timeouts: [],
+    };
 
+    componentDidMount() {
+        this.generateRandomArray();
+    }
+    generateRandomNumber = (min, max) => {
+        return Math.floor(Math.random() * (max-min) + min);
+    }
+
+    generateRandomArray = () => {
+        const count = this.state.count;
+        const temp = [];
+        for (let i =0; i < count; i++) {
+            temp.push(this.generateRandomNumber(50, 200));
+        }
+        this.setState({
+            array: temp,
+            arraySteps: [temp],
+        })
+    }
+    render() {
+        let bars = this.state.array.map((value, index) => (
+            <Bar 
+                key={index} 
+                index = {index} 
+                length = {value} 
+                color = {0}
+            />
+            )
+        );
+        return (
+            <div className = 'app'>
+                <div className = 'frame'>
+                    <div className='barsDiv container card'>{bars}</div>
+                </div>
+                <div className = 'control-panel'></div>
+                <div className='panel'></div>
+            </div>
+        );
+    }
+}
 export default App;
+
+
